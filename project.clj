cf59c13ba2e21d705a7b8d745349e1e4dea30ab7
Dan Pallas <git@danpallas.com>
2013-10-26 14:20:47 -0400

    Add dependency hiccup and update ring to 0.8.7

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject mvMusic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A server application for converting and downloading music with a web front end"
+  :url "https://github.com/DanPallas/mvMusic.git"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
-  :plugins [[lein-ring "0.8.5"]]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]]
+  :plugins [[lein-ring "0.8.7"]]
   :ring {:handler mvMusic.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}})
