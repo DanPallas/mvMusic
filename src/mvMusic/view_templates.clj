590596c8e7b371fb2b285658eb8c51dedf77e9f7
Dan Pallas <business@danpallas.com>
2013-11-01 17:09:06 -0400

    add wrap-tag function and implement in templay functions

--- src/mvMusic/view_templates.clj
+++ src/mvMusic/view_templates.clj
@@ -3,31 +3,33 @@
         [mvMusic.file-ops]
         [mvMusic.configuration]))
 
+(defn wrap-tag
+  "Returns a hiccup vector with the given content tag and optional attribute
+  map"
+  [content tag & [attribute-map]]
+  (vec (filter #(not (nil? %1)) (list tag attribute-map content))))
+
 (defn head
   "Generate hiccup html for header
     params:
       title: string to use as title"
   [title]
-  [:head
-   [:title title]])
+  (wrap-tag (wrap-tag title :title) :head))
 
-(defn format-directories 
-  "format directories into table rows"
+(defn format-directories
   [path]
   (->> (directory-url-list path)
-       (map #(conj [:a {:href (second %1)}] (first %1)))
-       (vec)
-       (map #(conj [:td] %1))
-       (map #(conj [:tr] %1))))
+       (map #(wrap-tag (first %1) :a {:href (second %1)}))
+       (map #(wrap-tag %1 :td))
+       (map #(wrap-tag %1 :tr))))
 
 (defn format-files 
   "format files into table rows"
   [path]
   (->> (file-url-list path)
-       (map #(conj [:a {:href (second %1)}] (first %1)))
-       (vec)
-       (map #(conj [:td] %1))
-       (map #(conj [:tr] %1))))
+       (map #(wrap-tag (first %1) :a {:href (second %1)}))
+       (map #(wrap-tag %1 :td))
+       (map #(wrap-tag %1 :tr))))
 
 (defn body 
   "Generate body html"
@@ -37,3 +39,5 @@
    (vec (concat [:table {:border 1}] 
           (format-directories path)
            (format-files path)))])
+
+
