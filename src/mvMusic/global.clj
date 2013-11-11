0962ffd654928704ec9ef9ebd0d871c7c1225b71
Dan Pallas <business@danpallas.com>
2013-11-11 17:51:06 -0500

    add back end functions to zip folders.

--- src/mvMusic/global.clj
+++ src/mvMusic/global.clj
@@ -1,8 +1,12 @@
 (ns mvMusic.global)
 
+
+;---------------Constants---------------------------
 (def browse-path "/music/")
 (def download-path "/download/")
+(def zip-buff-length 4194304)
+;---------------End Constants------------------------
 
 (def cfg-map {:music-folder (str (System/getProperty "user.home") "/" "Music")
               :temp-folder 
-                (str (System/getProperty "user.home") "/" "mvMusic-tmp")})
+                (str (System/getProperty "user.home") "/" "mvMusic-tmp/")})
