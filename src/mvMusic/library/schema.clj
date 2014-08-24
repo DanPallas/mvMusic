a06671230bd86da3e3cacd8348ce40821a45449c
Dan Pallas <git@danpallas.com>
2014-08-24 14:26:28 -0400

    add db constructors and rework schema

--- src/mvMusic/library/schema.clj
+++ src/mvMusic/library/schema.clj
@@ -4,85 +4,114 @@
             [clojure.java.jdbc :as jdb]
             [mvMusic.log :as log]))
 
-(def db
+(defn get-db-spec 
+  [& path]
+  "return db spec map. If path is given then db spec for that path will be
+  returned, otherwise cfg-map will determine location."
   {:classname "org.h2.Driver"
    :subprotocol "h2"
-   :subname (str (:temp-folder cfg-map) 
-                 (if (= (last (:temp-folder cfg-map)) \/) 
-                   "" 
-                   \/) 
-                 "library")})
+   :subname (if (first path)
+              path
+              (str (:temp-folder cfg-map) 
+                   (if (= (last (:temp-folder cfg-map)) \/) 
+                     "" 
+                     \/) 
+                   "library"))})
 
 (def folders
   (jdb/create-table-ddl :folders
-                        [:folder "VARCHAR(1024)" "PRIMARY KEY"]
-                        [:mod_date :TIMESTAMP]
-                        [:image :bool]))
+                        [:folder "VARCHAR" "PRIMARY KEY"]
+                        [:mod_date :TIMESTAMP]))
 
 (def songs 
   (jdb/create-table-ddl :songs
-                        [:file "VARCHAR(1024) PRIMARY KEY"]
+                        [:file :nvarchar "PRIMARY KEY"]
                         [:track :integer]
-                        [:disc :integer]
-                        [:title "VARCHAR(1024)"]
-                        [:artist "VARCHAR(1024)"]
-                        [:time :integer]
-                        [:published :integer]
-                        [:extension "VARCHAR(1024)"]
-                        [:genre "VARCHAR(1024)"]
-                        [:folder "VARCHAR(1024)"]
-                        [:filename "VARCHAR(1024)"]
-                        [:path "VARCHAR(1024)"]
-                        [:mod_date :TIMESTAMP]
-                        [:album_title "VARCHAR(1024)"]
-                        [:album_artist "VARCHAR(1024)"]
-                        [:album_sort "VARCHAR(1024)"]
-                        [:image :bool]
-                        [:other :text]
+                        [:track_total :integer]
+                        [:disc_no :integer]
+                        [:disc_total :integer]
+                        [:title :nvarchar]
+                        [:artist :nvarchar]
+                        [:album :nvarchar]
+                        [:album_artist :nvarchar]
+                        [:year :integer]
+                        [:genre :nvarchar]
+                        [:comment :nvarchar]
+                        [:composer :nvarchar]
+                        [:original_artist :nvarchar]
+                        [:remixer :nvarchar]
+                        [:conductor :nvarchar]
+                        [:bmp :integer]
+                        [:grouping :nvarchar]
+                        [:isrc :nvarchar]
+                        [:record-label :nvarchar]
+                        [:encoder :nvarchar]
+                        [:lyricist :nvarchar]
+                        [:lyrics :nclob]
+                        [:artwork_mime :nvarchar]
+                        [:artwork_data :blob]
+                        [:bit_rate :integer]
+                        [:channels :integer]
+                        [:encoding_type :nvarchar]
+                        [:format :nvarchar]
+                        [:sample_rate :integer]
+                        [:length :integer]
+                        [:variable_bit_rate :boolean]
+                        [:folder :nvarchar]
+                        [:filename :nvarchar]
+                        [:mod_date :timestamp]
                         ["foreign key(folder) REFERENCES folders(folder)"]))
 
-(def downloads 
-  (jdb/create-table-ddl :downloads
-                        [:file "VARCHAR(1024) PRIMARY KEY"]
-                        [:name "VARCHAR(1024)"]
-                        [:created :timestamp]
-                        [:filetypes "VARCHAR(1024)"]
-                        [:bitrates "VARCHAR(1024)"]))
-
-(def songs-downloads 
-  (jdb/create-table-ddl :songs_downloads
-                        [:song_file (str "VARCHAR(1024) REFERENCES songs(file)"
-                                         " ON DELETE CASCADE")]
-                        [:download_file 
-                         (str "VARCHAR(1024) REFERENCES downloads(file)"
-                              " ON DELETE CASCADE")]))
+;(def downloads 
+;  (jdb/create-table-ddl :downloads
+;                        [:file "VARCHAR(1024) PRIMARY KEY"]
+;                        [:name "VARCHAR(1024)"]
+;                        [:created :timestamp]
+;                        [:filetypes "VARCHAR(1024)"]
+;                        [:bitrates "VARCHAR(1024)"]))
+;
+;(def songs-downloads 
+;  (jdb/create-table-ddl :songs_downloads
+;                        [:song_file (str "VARCHAR(1024) REFERENCES songs(file)"
+;                                         " ON DELETE CASCADE")]
+;                        [:download_file 
+;                         (str "VARCHAR(1024) REFERENCES downloads(file)"
+;                              " ON DELETE CASCADE")]))
 
 
 
 (def folder-idx "CREATE INDEX songs_folder ON songs (folder)")
 
-(defn create-db  
-  "Creates database and all tables and indexes at location in cfg-map. "
-  [db]
+(defn create-db!  
+  "Creates database and all tables and indexes using db-spec"
+  [db-spec]
   (try
-    (jdb/db-do-commands db folders songs downloads songs-downloads)
-    (jdb/db-do-commands db folder-idx)
+    (jdb/db-do-commands db-spec folders songs)
+    (jdb/db-do-commands db-spec folder-idx)
     (catch Exception ex
       (log/error ex "Error creating database")
       #_(System/exit (:db-failure exit-codes)))))
 
-(defn drop-db 
+(defn drop-db! 
   "Deletes database file if it exists. Returns true if database deleted. Nil if
   database didn't exist."
-  [db]
+  [db-spec]
   (try 
     (jdb/db-do-commands db "DROP ALL OBJECTS DELETE FILES")
     (catch Exception ex
       (log/error ex "Error creating database")
       #_(System/exit (:db-failure exit-codes)))))
 
-(defn recreate-db 
+(defn recreate-db! 
   "Deletes database and then recreates all tables and indexes without data."
-  [db]
-  (drop-db db)
-  (create-db db))
+  [db-spec]
+  (drop-db! db-spec)
+  (create-db! db-spec))
+
+(defn get-db!
+  "Get db-spec and create db if it doesn't already exist."
+  [& db-spec]
+  (let [db-spec (if (empty? db-spec)  (get-db-spec) (first db-spec))]
+    (when-not (.exists (io/as-file (str (:subname (get-db-spec)) ".mv.db"))) 
+      (create-db! db-spec))
+    db-spec))
